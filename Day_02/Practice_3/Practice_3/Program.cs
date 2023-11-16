ade653966f07ada99a9e4203d7ea6649eca67dff
dea tchkoidze <chkoidze.dea@gmail.com>
2023-11-16 15:40:37 +0400

    finish Practie_03

---
+++ Day_02/Practice_3/Practice_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System;
+
+internal class Program
+{
+   
+    private static void Main(string[] args)
+    {
+       
+        bool inputForSquare = false;
+        while (!inputForSquare)
+        {
+            Console.WriteLine("Enter  Number :");
+            string sqrtNumber = Console.ReadLine();
+            if (int.TryParse(sqrtNumber, out int numberSquare))
+            {
+                int result = (int)Math.Pow(numberSquare, 2);
+                Console.WriteLine($"The pow of the entered Number is {result}");
+                inputForSquare = true;
+            }
+            else
+            {
+                Console.WriteLine("Please enter valid input for  Num");
+            }
+            Console.ReadLine();
+        }
+       
+    }
+
+}
+
+
+
+
