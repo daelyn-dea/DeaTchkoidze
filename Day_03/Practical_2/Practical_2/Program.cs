e329f6a433657f0a4ecbbc2a16c8d6ab8012c98c
dea tchkoidze <chkoidze.dea@gmail.com>
2023-11-16 16:06:13 +0400

    finish Practical_2

---
+++ Day_03/Practical_2/Practical_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int year = Convert.ToInt32(Console.ReadLine());
+if (year % 4 == 0)
+{
+    Console.WriteLine(true);
+}
+else
+{
+    Console.WriteLine(false);
+}
