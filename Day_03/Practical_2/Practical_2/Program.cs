284a9d32233c89fe57d28b62fa5abe5593199bfe
dea tchkoidze <chkoidze.dea@gmail.com>
2023-11-16 19:33:38 +0400

    Practical_2 done (change logic of years)

--- Day_03/Practical_2/Practical_2/Program.cs
+++ Day_03/Practical_2/Practical_2/Program.cs
@@ -1,6 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int year = Convert.ToInt32(Console.ReadLine());
-if (year % 4 == 0)
+if(year % 100 == 0)
+{
+    if(year % 400 == 0)
+    {
+        Console.WriteLine(true);
+    }
+    Console.WriteLine(false);
+}
+ else if (year % 4 == 0)
 {
     Console.WriteLine(true);
 }
