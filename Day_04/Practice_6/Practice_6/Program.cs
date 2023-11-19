46c929de642c6da8e8c10082ea2ba4889dd4736a
dea tchkoidze <chkoidze.dea@gmail.com>
2023-11-19 18:42:59 +0400

    finished Pracite_4

---
+++ Day_04/Practice_6/Practice_6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter a number : ");
+int input = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= input; i++)
+{
+   if ( input % i == 0)
+    Console.Write(i + ", ");
+}
---
+++ Day_04/Practice_8/Practice_8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter a number : ");
+int input = Convert.ToInt32(Console.ReadLine());
+string output = " ";
+for (int i = 0; i <= input; i++)
+{
+    for(int j = 0; j <= input; j++) 
+    {
+        int binary = input % 2;
+        output = binary + output;
+        input /= 2;
+    }
+}
+Console.WriteLine(output);
+Console.ReadLine();
