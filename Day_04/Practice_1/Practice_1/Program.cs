c402691bfa3295082a9e5b61980b14f563fcf17a
dea tchkoidze <chkoidze.dea@gmail.com>
2023-11-19 18:13:09 +0400

    finished practices_1

---
+++ Day_04/Practice_1/Practice_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int sum = 0;
+for (int i = 0; i < 10; i++)
+{
+    sum += i;
+}
+Console.WriteLine($"Sum from 0 to 10 is {sum}"); // 10 მდე ჯამი 45-ია
+sum = 0;
+for (int i = 0; i <= 10; i++)
+{
+    sum += i;
+}
+Console.WriteLine($"Sum from 0 to 10 is {sum}");//10-ის ჩათვლით - 55
