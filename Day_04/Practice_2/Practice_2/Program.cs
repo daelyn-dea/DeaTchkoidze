7bc3e4be3bf6091027f09c0bc4b41df52d616403
dea tchkoidze <chkoidze.dea@gmail.com>
2023-11-19 18:41:06 +0400

    finished  Practiced_2

---
+++ Day_04/Practice_2/Practice_2/Program.cs
@@ -0,0 +1,8 @@
+﻿int sum = 0;
+Console.Write("Enter a number: ");
+int input = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i <= input; i++)
+{
+    sum += i;
+}
+Console.WriteLine($"Sum from 0 to {input} is {sum}");
