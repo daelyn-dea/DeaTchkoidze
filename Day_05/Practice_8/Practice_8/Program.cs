463d8ff66f4e769f0d4eb18a8fdd67382e0c1947
dea tchkoidze <chkoidze.dea@gmail.com>
2023-11-23 16:03:34 +0400

    Finished Practice_08

---
+++ Day_05/Practice_8/Practice_8/Program.cs
@@ -0,0 +1,35 @@
+﻿int[,] array1 = new int[8, 8];
+int[,] array2 = new int[8, 8];
+int row = 7;
+
+for (int i = 0; i < 8; i++)
+{
+    for (int j = 7; j > i; j--)
+    {
+        array1[i, j] = 1;
+    }
+}
+
+for (int i = 0; i < 8; i++)
+{
+    for (int j = 0; j < 8; j++)
+    {
+        Console.Write(array1[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("\n");
+
+for (int i = 0; i < 8; i++)
+{
+    for (int j = 0; j < 8; j++)
+    {
+        array2[i, j] = array1[row, j];
+        Console.Write(array2[i, j] + " ");
+    }
+    row--;
+    Console.WriteLine();
+}
+
+Console.ReadLine();
