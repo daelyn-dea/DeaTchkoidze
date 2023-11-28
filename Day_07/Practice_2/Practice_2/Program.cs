aa4f26312f4ac400d4c0c3b8b9270d44387222b6
dea tchkoidze <chkoidze.dea@gmail.com>
2023-11-28 22:32:43 +0400

    Finished Practice_02

---
+++ Day_07/Practice_2/Practice_2/Program.cs
@@ -0,0 +1,26 @@
+﻿string text = EnterText();
+ReverseTextByMethod(text);//მეთოდის გამოყენებით
+ReverseText(text);//ფორით
+Console.Read();
+
+
+void ReverseTextByMethod(string text)
+{
+    string reversed = new string(text.Reverse().ToArray());
+    Console.WriteLine(reversed);
+}
+
+void ReverseText(string text)
+{
+    for (int i = text.Length - 1; i >= 0; i--)
+    {
+        Console.Write(text[i]);
+    }
+}
+
+string EnterText()
+{
+    Console.Write("Please enter the text: ");
+    string text = Console.ReadLine()!;
+    return text!;
+}
