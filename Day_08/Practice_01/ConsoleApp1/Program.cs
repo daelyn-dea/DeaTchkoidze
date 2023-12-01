fcbb599b42efdd997c5d3e6a7228100950798743
dea tchkoidze <chkoidze.dea@gmail.com>
2023-12-01 15:41:45 +0400

    Finished Practice_01

---
+++ Day_08/Practice_01/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Please enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Printed by recursion: ");
+PrintNumbers(num);
+Console.WriteLine();
+
+Console.Write("Printed by tail recursion: ");
+PrintNumbersTail(num, 1);
+Console.Read();
+
+void PrintNumbers(int num)
+{
+    if (num > 0)
+    {
+        PrintNumbers(num - 1);
+        Console.Write($"{num} ");
+    }
+}
+void PrintNumbersTail(int num, int one)
+{
+    if (one <= num)
+    {
+        Console.Write($"{one} ");
+        PrintNumbersTail(num, one + 1);
+    }
+}
