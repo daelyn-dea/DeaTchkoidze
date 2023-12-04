96194f88e0c6d29556aeaa8927b671fe0119b0a2
dea tchkoidze <chkoidze.dea@gmail.com>
2023-12-04 14:05:29 +0400

    Finished Practice_02

---
+++ Day_09/Practice_02/Practice_02/Program.cs
@@ -0,0 +1,19 @@
+﻿using _Triangle;
+
+Triangle triangle = new Triangle();
+Console.Write("Enter Side 1: ");
+triangle.Side1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Side 2: ");
+triangle.Side2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Side 3: ");
+triangle.Side3 = Convert.ToInt32(Console.ReadLine());
+if (triangle.ValidateTriangle())
+{
+    Console.WriteLine($"Perimeter of the triangle is: {triangle.CalculatePerimeter()}");
+    Console.WriteLine($"Area of the triangle is: {triangle.CalculateArea()}");
+}
+else
+{
+    Console.WriteLine("It isn't valid Triangle");
+}
+Console.Read();
---
+++ Day_09/Practice_02/Practice_02/Triangle.cs
@@ -0,0 +1,70 @@
+﻿
+namespace _Triangle
+{
+    internal class Triangle
+    {
+        int _Side1;
+        int _Side2;
+        int _Side3;
+
+        public int Side1
+        {
+            get
+            {
+                return _Side1;
+            }
+            set
+            {
+                if(value > 0)
+                {
+                    _Side1 = value;
+                }
+            }
+        }
+        public int Side2
+        {
+            get
+            {
+                return _Side2;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _Side2 = value;
+                }
+            }
+        }
+        public int Side3
+        {
+            get
+            {
+                return _Side3;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _Side3 = value;
+                }
+            }
+        }
+        public double CalculateArea()
+        {
+            double s = (_Side1 + _Side2 + _Side3) / 2;
+            return Math.Sqrt(s * (s - _Side1) * (s - _Side2) * (s - _Side3));
+        }
+        public double CalculatePerimeter()
+        {
+            return Side1 + Side2 + Side3;
+        }
+        public bool ValidateTriangle()
+        {
+            if(_Side1+_Side2>_Side3 && _Side1+_Side3>_Side2 && _Side2 + _Side3 > _Side1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
