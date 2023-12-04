3cdfaa25c87377c6ec22700c35488bced5dcdf1e
dea tchkoidze <chkoidze.dea@gmail.com>
2023-12-04 14:03:56 +0400

    Finished Practice_01

---
+++ Day_09/Practice_01/Practice_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using _Cat;
+
+Console.WriteLine("Creating cat object");
+Cat cat1 = new Cat();
+Console.Write("Enter Name: ");
+cat1.Name = Console.ReadLine();
+Console.Write("Enter Breed : ");
+cat1.Breed = Console.ReadLine();
+Console.Write("Enter Age : ");
+cat1.Age = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Sex : ");
+cat1.Sex = Console.ReadLine();
+Console.WriteLine("Cat object created");
+Console.Write("Enter food weight in grams: ");
+int grams = Convert.ToInt32(Console.ReadLine());
+cat1.Eating(grams);
+Console.Write("Enter Meowing count: ");
+int meowingcount = Convert.ToInt32(Console.ReadLine());
+cat1.Meowing(meowingcount);
