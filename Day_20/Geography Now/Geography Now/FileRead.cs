36b9edc9b6baa8f92f35aba8e061c422a6fd9d3a
dea tchkoidze <chkoidze.dea@gmail.com>
2024-01-07 21:43:52 +0400

    Finished homework Day_20

---
+++ Day_20/Geography Now/Geography Now/FileRead.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+
+namespace Geography_Now_
+{
+    internal static class FileRead
+    {
+        internal static List<City> fileReadMethod()
+        {
+            List<City> cityList = new List<City>();
+
+            string path = @"C:\Users\dchko\Desktop\visual studio\.NET\Day_20\Cities.txt";
+            
+                using (StreamReader sr = new StreamReader(path))
+                {
+
+                    var line = sr.ReadLine();
+                    if (line != null)
+                    {
+                    string[] array1 = line.Split('|');
+                    while (line != null && array1.Length > 1)
+                        {
+                                City cities = new City()
+                                {
+                                    Name = array1[0],
+                                    Area = Convert.ToDecimal(array1[1], new CultureInfo("fr-FR")),
+                                    Popualtion = Convert.ToInt32(array1[2]),
+                                    Capital = Convert.ToBoolean(array1[3]),
+                                    Country = array1[4]
+                                };
+
+                                cityList.Add(cities);
+
+                                line = sr.ReadLine();
+                                array1 = line.Split('|');
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception($"File is empty");
+                    }
+                }
+                return cityList;
+         }     
+     }
+ }
---
+++ Day_20/Geography Now/Geography Now/GeographicEntity.cs
@@ -0,0 +1,77 @@
+﻿namespace Geography_Now_
+{
+    internal abstract class GeographicEntity
+    {
+        public string Name { get; set; }
+        public int Popualtion { get; set; }
+        public decimal Area { get; set; }
+    }
+    internal class City : GeographicEntity
+    {
+        public bool Capital { get; set; }
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+           
+                return $"Name : {Name}, Area : {Area}, is capital city : {Capital},Country : {Country},  Popualtion : {Popualtion}";
+           
+
+        }
+
+    }
+    internal class Country : GeographicEntity
+    {
+        public Country(string name, List<City> cityes) 
+        {
+            Cityes = cityes;
+            Name = name;
+            Area = CalculateArea();
+            Popualtion = CalculatePopulation();
+        }
+        public string CapitalCity { get; set; }
+       
+        List<City> Cityes { get; set; }
+        public decimal CalculateArea()
+        {
+            decimal countArea = 0; 
+            if( Cityes != null && Cityes.Count > 0 )
+            {
+                foreach(City city in Cityes)
+                {
+                    countArea += city.Area;
+                }
+            }
+            return countArea;
+        }
+        public int CalculatePopulation()
+        {
+            int countPopulation = 0;
+            if(Cityes != null && Cityes.Count > 0)
+            {
+                foreach(City city in Cityes)
+                {
+                    countPopulation += city.Popualtion;
+                }
+            }
+            return countPopulation; 
+        }
+        public override string ToString()
+        {
+            try
+            {
+                string cities = Cityes[0].Name;
+                for (int i = 1; i < Cityes.Count; i++)
+                {
+                    cities += ", " + Cityes[i].Name;
+                }
+                return $"Name : {Name}, Area : {Area}, cities : {cities}, Popualtion : {Popualtion}";
+            }
+            catch
+            {
+                throw new  Exception("Your entered invalid country name!");
+            }
+        }
+
+    }
+}
