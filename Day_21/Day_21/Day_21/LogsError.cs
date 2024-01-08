be3309591fff92d1d491ec9b94ba79104a2e05c6
dea tchkoidze <chkoidze.dea@gmail.com>
2024-01-08 18:14:59 +0400

    Finished Testing Time

---
+++ Day_21/Day_21/Day_21/LogsError.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_21
+{
+    internal static class ErrorLog
+    {
+        internal static void ErrorLogInFile(string errorMessage, string errorStackTrace)
+        {
+            string logFilePath = @"C:\Users\dchko\Desktop\visual studio\.NET\Day_21\Logs.txt";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(logFilePath, true))
+                {
+                    sw.WriteLine($"{DateTime.Now}: {errorMessage}: StackTrace: {errorStackTrace}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Day_21/Day_21/Day_21/Program.cs
@@ -0,0 +1,24 @@
+﻿using Day_21;
+
+Console.WriteLine("Choose one: 1) Start Test 2) Add Test.");
+try
+{
+    string choose = Console.ReadLine();
+    if (choose == "Start Test")
+    {
+        ReadTestFromFile.ReadFileMethod();
+    }
+    else if (choose == "Add Test")
+    {
+        WriteTestInFile.CallCreateTest();
+    }
+    else
+    {
+        throw new Exception("Enter just Start Test or Add Test");
+    }
+
+}
+catch(Exception ex)
+{
+    ErrorLog.ErrorLogInFile(ex.Message, ex.StackTrace);
+}
