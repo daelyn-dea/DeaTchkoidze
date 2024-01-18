334fe03a323eb334744d0a3dbdc1e052a26b5f42
dea tchkoidze <chkoidze.dea@gmail.com>
2024-01-18 15:54:43 +0400

    finished practice

---
+++ Day_24/Practice/Practice/Customers.cs
@@ -0,0 +1,32 @@
+﻿namespace Practice
+{
+    internal class Customer
+    {
+        public Customer(int customerID, string name)
+        {
+            CustomerID = customerID;
+            Name = name;
+        }
+
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+    }
+
+    internal static class CustomerMaker
+    {
+        public static List<Customer> CustomerListMakerMethod()
+        {
+            string path = @".\Customers.txt";
+
+            List<Customer> customersList = new();
+
+            IEnumerable<string[]> customersinfo = FileReader.FilereaderMethod(path);
+            foreach(var item in customersinfo)
+            {
+                customersList.Add(new Customer(Convert.ToInt32(item[0]), item[1]));
+            }
+
+            return customersList;
+        }
+    }
+}
---
+++ Day_24/Practice/Practice/Program.cs
@@ -0,0 +1,25 @@
+﻿using Practice;
+
+List<Customer> customers = CustomerMaker.CustomerListMakerMethod();
+List<Order> orders = OrderMaker.OrdersListMaker();
+
+var avarageOrderMoreThen10MethodSyntax = Reporter.AvarageOrderMoreThen10MethodSyntax(orders, customers);
+var minAmountPerOrderQuerySyntax = Reporter.MinAmountPerOrderQuerySyntax(orders, customers);
+var oderCountPerPErsonMethodSyntax = Reporter.OderCountPerPErsonMethodSyntax(orders, customers);
+var reportOfCountOrdersMethodSyntax = Reporter.ReportOfCountOrdersMethodSyntax(orders, customers);
+var sumAmountPerPErsonMethoSuntax = Reporter.SumAmountPerPErsonMethoSuntax(orders, customers);
+
+
+foreach(var item in customers.Select(x => x.CustomerID))
+{
+    string averageOrderMoreThen10 = avarageOrderMoreThen10MethodSyntax.ContainsKey(item) ? $"Order average {avarageOrderMoreThen10MethodSyntax[item]}" : "Hasn't ordeer more then 10";
+    string minAmountPerOrder = minAmountPerOrderQuerySyntax.ContainsKey(item) ? $"Min amount {minAmountPerOrderQuerySyntax[item]:C2}" : "Hasn't order";
+    string oderCountPerPErson = oderCountPerPErsonMethodSyntax.ContainsKey(item) ? $"Order count per person : {oderCountPerPErsonMethodSyntax[item]}" : "Hasn't order";
+    string reportOfCountOrders = reportOfCountOrdersMethodSyntax.ContainsKey(item) ? $"Has more then 1 order {reportOfCountOrdersMethodSyntax[item]}" : "Has less then one order";
+    string sumAmountPerPErson = sumAmountPerPErsonMethoSuntax.ContainsKey(item) ? $"Sum amount : {sumAmountPerPErsonMethoSuntax[item]:C2}" : "Hasn't order";
+
+
+
+
+    Console.WriteLine($"{item},  {averageOrderMoreThen10} | {minAmountPerOrder} | {oderCountPerPErson} | {reportOfCountOrders} | {sumAmountPerPErson}");
+}
