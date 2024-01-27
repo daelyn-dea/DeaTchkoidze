22d5f5458de98792c71c4e6cb38fda50e82d6ee4
dea tchkoidze <chkoidze.dea@gmail.com>
2024-01-27 20:20:11 +0400

    Finished homework day 29

---
+++ Day_29/Practice/Practice/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public char Group { get; set; }
+        public string FirstName { get; set; }
+    }
+
+}
